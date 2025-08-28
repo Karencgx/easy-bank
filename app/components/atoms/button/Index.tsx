de38f342f4e29bb6e89dc8a93029078fa17d29a4
Karen <karen.cardonag@udea.edu.co>
2025-08-28 16:24:46 -0500

    boton

---
+++ app/components/atoms/button/Index.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const Index = () => {
+  return (    
+  <button className = "h-11 w-40 gradient-bg text-white font-bold  rounded-full hover:opacity-70 hover:cursor-pointer transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+    Request invite
+    </button>
+  );
+};
+
+export default Index;
